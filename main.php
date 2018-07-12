6a83349a931db7f78def7b6a56de34b608339a2d
onaga04 <31213368+onaga04@users.noreply.github.com>
2018-07-12 11:32:06 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+   <meta charset="UTF-8">
+   <title>Homepage</title>
+</head>
+<body>
+<h2>User Authentication System </h2><hr>
+
+<p>You are currently not signed in. <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a></p>
+
+<p>You are logged in as {username} <a href="logout.php">Logout</a></p>
+<!-- --><?php //include_once 'resource/Database.php' ?><!-- -->
+</body>
+</html>
