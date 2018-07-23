69f040d0ea0d1be13a73c197fb174be0f9a67a0c
onaga04 <31213368+onaga04@users.noreply.github.com>
2018-07-23 10:12:20 -0400

    Update create-loan.php

--- include/create-loan.php
+++ include/create-loan.php
@@ -1,1 +1,22 @@
-
+<?php
+  <!Doctype HTML>
+    <html>
+    <form action="/index/index.php">
+            First name:<br>
+            <input type="text" name="firstname" value="Mickey">
+            <br>
+            Last name:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br>
+            A number:<br>
+            <input type="text" name="a_number" value="A00948759">
+            <br>
+            email:<br>
+            <input type="text" name="email" value="dfubbfefu@gmail.com">
+            clothing:<br>
+            <input type="text" name="description" value="dress pants">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </html>
+?>
