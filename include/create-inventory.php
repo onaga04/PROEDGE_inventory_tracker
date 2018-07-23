0e9e8f7e64e9dd82aa23b9ceadfa8089ccfa86b0
onaga04 <31213368+onaga04@users.noreply.github.com>
2018-07-23 10:17:17 -0400

    Update create-inventory.php

--- include/create-inventory.php
+++ include/create-inventory.php
@@ -1,1 +1,18 @@
+<?php
+  <Doctype HTML>
+    <html>
+    <div id = 'create_loan' class = 'labelcontent'>
+            <form action="/main.php">
+               Description:<br>
+                <input type="text" name="description" value="dress pants">
+                <br>
+               Color:<br>
+                <input type="text" name="color" value="black">
+                <br><br>
+            <input type="submit" value="Submit">
+            </form>
+            <!-- //Submit Button -->
+        </div>
 
+    </html>
+?>
