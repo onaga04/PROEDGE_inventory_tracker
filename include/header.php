1cffcb52de74d6687fc7ec7c940aaa333f81092e
onaga04 <31213368+onaga04@users.noreply.github.com>
2018-07-23 09:47:37 -0400

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,1 +1,26 @@
+<?php
+ <!Doctype HTML>
+      <html>
+        <head>
+            <title>Page Title</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+            <!-- jQuery library -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </head>
+
+        <body>
+
+          <header>
+            <h1 class="text-center">PRO-EDGE INVENTORY TRACKER  <hi>
+          </header>
+         </body>
+      </html>
+   ?>
+   
 
